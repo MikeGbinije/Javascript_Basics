85b55e2cfee0817f87a682b87cf79ae4a9861e0a
Mike Gbinije <mikegbinije@Mikes-MacBook-Air.local>
2024-10-30 22:24:20 -0500

    first upload

---
+++ variable.js
@@ -0,0 +1,24 @@
+const name = "Mike"; 
+let age = 32; 
+const isSmart = true; //this is a boolean
+// ------------------------------------------
+let hobby = "working out"
+hobby = "watching tv"
+// ------------------------------------------
+let x = 10;
+let y = 5;
+
+let sum = x + y; // Addition
+let difference = x - y; // Subtraction
+let product = x * y; // Multiplication
+let quotient = x / y; // Division
+let remainder = x % y; // Remainder / modulus
+//--------------------------------------------
+let a = 50;
+let b = 25;
+
+let dog = a % b; // Calculate remainder
+let doubleSum = (a + b) * 2; // Add and double the result
+
+console.log(dog); // Print remainder
+console.log(doubleSum); // Print doubleSum
